46018701142f6938bb0363369c5e7217d163337f
malexanderboyd <m.alexander.boyd@gmail.com>
2015-06-23 12:44:54 -0400

    Basic Launcher
    rev 0.1
    Basic Launcher:
    - Hard Coded File paths (plan to save these in user settings)
    - Can't move application
    - shitty art, etc

---
+++ privateServerLauncher/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cataLauncher = New System.Windows.Forms.Button()
+        Me.wotlkLauncher = New System.Windows.Forms.Button()
+        Me.bcLauncher = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cataLauncher
+        '
+        Me.cataLauncher.Location = New System.Drawing.Point(580, 356)
+        Me.cataLauncher.Name = "cataLauncher"
+        Me.cataLauncher.Size = New System.Drawing.Size(75, 23)
+        Me.cataLauncher.TabIndex = 0
+        Me.cataLauncher.Text = "Cataclysm"
+        Me.cataLauncher.UseVisualStyleBackColor = True
+        '
+        'wotlkLauncher
+        '
+        Me.wotlkLauncher.Location = New System.Drawing.Point(299, 356)
+        Me.wotlkLauncher.Name = "wotlkLauncher"
+        Me.wotlkLauncher.Size = New System.Drawing.Size(136, 23)
+        Me.wotlkLauncher.TabIndex = 1
+        Me.wotlkLauncher.Text = "WOTLK"
+        Me.wotlkLauncher.UseVisualStyleBackColor = True
+        '
+        'bcLauncher
+        '
+        Me.bcLauncher.Location = New System.Drawing.Point(31, 354)
+        Me.bcLauncher.Name = "bcLauncher"
+        Me.bcLauncher.Size = New System.Drawing.Size(136, 23)
+        Me.bcLauncher.TabIndex = 2
+        Me.bcLauncher.Text = "Burning Crusade"
+        Me.bcLauncher.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.Location = New System.Drawing.Point(683, 2)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(22, 19)
+        Me.exitButton.TabIndex = 3
+        Me.exitButton.Text = "X"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.privateServerLauncher.My.Resources.Resources.tumblr_mo1j1iVamH1qmc8m1o1_500
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(717, 391)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.bcLauncher)
+        Me.Controls.Add(Me.wotlkLauncher)
+        Me.Controls.Add(Me.cataLauncher)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents cataLauncher As System.Windows.Forms.Button
+    Friend WithEvents wotlkLauncher As System.Windows.Forms.Button
+    Friend WithEvents bcLauncher As System.Windows.Forms.Button
+    Friend WithEvents exitButton As System.Windows.Forms.Button
+
+End Class
---
+++ privateServerLauncher/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub bcLauncher_Click(sender As System.Object, e As System.EventArgs) Handles bcLauncher.Click
+        ' Dim realmPath As String = Nothing
+        Dim realmName As String = Nothing
+        ' realmPath = InputBox("Enter your Realmlist Path.", "Realmlist Path", , , )
+        realmName = InputBox("Enter your new Realmlist.", "Realmlist Edit", , , )
+
+        If Not String.IsNullOrEmpty(realmName) Then
+            FileOpen(1, "F:\Programs\World of Warcraft 2.4.3\realmlist.wtf", OpenMode.Output)
+            PrintLine(1, "set realmlist " + realmName)
+            FileClose(1)
+        End If
+
+        System.Diagnostics.Process.Start("F:\Programs\World of Warcraft 2.4.3\wow.exe")
+    End Sub
+
+    Private Sub wotlkLauncher_Click(sender As System.Object, e As System.EventArgs) Handles wotlkLauncher.Click
+        '  Dim realmPath As String = Nothing
+        Dim realmName As String = Nothing
+        ' realmPath = InputBox("Enter your Realmlist Path.", "Realmlist Path", , , )
+        realmName = InputBox("Enter your new Realmlist.", "Realmlist Edit", , , )
+
+        If Not String.IsNullOrEmpty(realmName) Then
+            FileOpen(1, "F:\Programs\World of Warcraft 3.3.5\Data\enUS\realmlist.wtf", OpenMode.Output)
+            PrintLine(1, "set realmlist " + realmName)
+            FileClose(1)
+        End If
+
+        System.Diagnostics.Process.Start("F:\Programs\World of Warcraft 3.3.5\wow.exe")
+    End Sub
+
+    Private Sub cataLauncher_Click(sender As System.Object, e As System.EventArgs) Handles cataLauncher.Click
+
+        ' Dim realmPath As String = Nothing
+        Dim realmName As String = Nothing
+        ' realmPath = InputBox("Enter your Realmlist Path.", "Realmlist Path", , , )
+        realmName = InputBox("Enter your new Realmlist.", "Realmlist Edit", , , )
+
+        If Not String.IsNullOrEmpty(realmName) Then
+            FileOpen(1, "F:\Programs\World ofWarcraft 4.3.4\Data\enGB\realmlist.wtf", OpenMode.Output) 'Hard coded in because I'm too lazy to find path always for personal use.'
+            PrintLine(1, "set realmlist " + realmName)
+            FileClose(1)
+        End If
+        System.Diagnostics.Process.Start("F:\Programs\World ofWarcraft 4.3.4\WoW-x64.exe")
+    End Sub
+
+    Private Sub exitButton_Click(sender As System.Object, e As System.EventArgs) Handles exitButton.Click
+        Me.Close()
+    End Sub
+End Class
